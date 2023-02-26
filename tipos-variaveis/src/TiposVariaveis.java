c7664be44dd5460f3329bd6e4e9ed2166f13f9b5
Thiago S Lacerda <thiago_lacerda10@hotmail.com>
2023-02-26 00:04:36 -0300

    criando um primeiro projeto sincronizado c/ vscode

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,7 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("esse é meu projeto de teste do vscode");
+    }
+}
+
